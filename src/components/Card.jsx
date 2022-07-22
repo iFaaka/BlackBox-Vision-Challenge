4952e86b72c3e0a46384225d1ae8e025bb3e458e
iFaaka <alvarezfacundo321@gmail.com>
2022-07-22 14:01:01 -0300

    Semi-finish

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./stylesheets/Card.css";
 export const Card = ({
   question,
   categ,
@@ -10,25 +10,43 @@ export const Card = ({
   setScore,
   type,
 }) => {
+  const [isCorrect, setIsCorrect] = useState(0);
   const handleClick = (ans) => {
     if (ans === correct) {
       if (type === "boolean") {
-        setScore((prevS) => prevS + 5);
-      } else {
-        setScore((prevS) => prevS + 10);
+        setIsCorrect(1);
+        setTimeout(() => {
+          setIsCorrect(0);
+          setScore((prevS) => prevS + 5);
+          nextQuestion((prevS) => prevS + 1);
+        }, 1000);
+      } else if (type === "multiple") {
+        setIsCorrect(1);
+        setTimeout(() => {
+          setIsCorrect(0);
+          setScore((prevS) => prevS + 10);
+          nextQuestion((prevS) => prevS + 1);
+        }, 1000);
       }
+    } else {
+      setTimeout(() => {
+        nextQuestion((prevS) => prevS + 1);
+      }, 500);
     }
-    nextQuestion((prevS) => prevS + 1);
   };
-
+  console.log(answers);
   return (
-    <div>
+    <div className="card-container">
       <p>{question}</p>
       <p>Category: {categ}</p>
       <p>Difficulty: {diffi}</p>
       <div className="button-container">
         {answers.map((ans) => (
-          <button key={ans} onClick={() => handleClick(ans)}>
+          <button
+            className={`${isCorrect === 1 && ans === correct ? "correct" : ""}`}
+            key={ans}
+            onClick={() => handleClick(ans)}
+          >
             {ans}
           </button>
         ))}
--- src/components/QuestionGame.jsx
+++ src/components/QuestionGame.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { Card } from "./Card";
 import { getData } from "./getData";
-
+import "./stylesheets/QuestionGame.css";
+var he = require("he");
 export const QuestionGame = () => {
   const [data, setData] = useState([]);
   const [questionNumber, setQuestionNumber] = useState(0);
   const [score, setScore] = useState(0);
+  const [gamesCount, setGamesCount] = useState(1);
   useEffect(() => {
     async function awaitData() {
       let myData = await getData();
       setData(myData);
     }
     awaitData();
-  }, []);
+  }, [gamesCount]);
   console.log(score);
   let fragment;
 
@@ -20,25 +22,52 @@ export const QuestionGame = () => {
     console.log(data);
     fragment = (
       <Card
-        question={data[0][questionNumber].question}
+        question={he.decode(data[0][questionNumber].question)}
         answers={[
-          ...data[0][questionNumber].incorrect_answers,
-          data[0][questionNumber].correct_answer,
+          ...data[0][questionNumber].incorrect_answers.map((item) =>
+            he.decode(item)
+          ),
+          he.decode(data[0][questionNumber].correct_answer),
         ].sort(() => Math.random() - 0.5)}
         diffi={data[0][questionNumber].difficulty}
-        correct={data[0][questionNumber].correct_answer}
-        categ={data[0][questionNumber].category}
+        correct={he.decode(data[0][questionNumber].correct_answer)}
+        categ={he.decode(data[0][questionNumber].category)}
         type={data[0][questionNumber].type}
         nextQuestion={setQuestionNumber}
         setScore={setScore}
       />
     );
+  } else if (10 === questionNumber) {
+    fragment = (
+      <div className="finish-container">
+        <h2>We are done! Your score was: {score}/100</h2>
+        <span>You plays {gamesCount} times</span>
+        <button
+          onClick={() => {
+            setGamesCount((prevS) => prevS + 1);
+            setTimeout(() => {
+              setQuestionNumber(0);
+              setScore(0);
+            }, 2000);
+          }}
+        >
+          Volver a empezar
+        </button>
+      </div>
+    );
   } else {
-    fragment = <h2>Loading...</h2>;
+    fragment = <h4>Loading...</h4>;
   }
   return (
-    <div>
-      <h1>QuestionGame</h1>
+    <div className="question-game-container">
+      <div className="title-container">
+        <h1>QuestionGame</h1>
+        <div className="subtitle-container">
+          <h2>Powered by:</h2>
+          <img src="https://res.cloudinary.com/blackboxvision/image/upload/f_auto,c_limit,w_256,q_auto/v1624760386/website/logos/logo-transparent_vzcuce.png"></img>
+        </div>
+      </div>
+
       {fragment}
     </div>
   );
